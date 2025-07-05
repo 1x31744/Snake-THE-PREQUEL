62e1a804ada59414c685b8aaf9803bc4153e7d7a
kxxpa <koopahello@gmail.com>
2025-07-05 19:06:34 +0100

    started printing and gotten some good resources

--- snake.asm
+++ snake.asm
@@ -1,19 +1,54 @@
-;setup
+;! for interrupts: http://employees.oneonta.edu/higgindm/assembly/DOS_AND_ROM_BIOS_INTS.htm
+
 start:
     mov ax, 07C0h
     mov ds, ax
 
-    jmp simple_animation
+    call refresh_screen
 
-simple_animation:
+    jmp $
+
+refresh_screen:
+    call clear_screen
+
+    call draw_top_border
+
+    ret
+
+draw_top_border:
+    ;setting the cursor position
+
+    mov ah, 02h
+    mov bh, 0
+    mov dh, 0
+    mov dl, 0
+    int 10h
+
+    mov si, horizontal_border_string
+    call print_string
+
+print_string:
+    lodsb
+    cmp al, 0
+    je done_printing
+    mov ah, 0Eh
+    int 10h
+    jmp print_string
+done_printing:
+    ret
+
+clear_screen:
+    mov ah, 06h       ; Scroll up function
+    mov al, 0         ; 0 = scroll entire window (clear screen)
+    mov bh, 07h       ; Attribute for blank lines (white text on black background)
+    mov cx, 0         ; upper-left corner (row=0, col=0)
+    mov dx, 184Fh     ; lower-right corner (row=24, col=79)
+    int 10h
+    ret
     
 ;constants
-
-
+horizontal_border_string db 'Hello World!', 0
 ;variables
 
 ;game logic
-
-;bootsector padding
-times 510-($-$$) db 0 ; pads the rest of the bootloader with 510 bytes, aiming for a 512 byte bootloader
-dw 0xAA55 ; specifies the end of the bootloader, recognised by bios
+times 2048-($-$$) db 0
